7f6dc377a18ec3a5c96e0d7f5548cf059a25134b
Mintmile <padalicapavel@gmail.com>
2026-09-10 14:12:08 +0300

    Update App.java

--- Snake_/Snake_/App.java
+++ Snake_/Snake_/App.java
@@ -1,1 +1,24 @@
+import Views.GamePanel;
+import javax.swing.*;
 
+/**
+ * Точка входа приложения.
+ */
+public class App {
+    public static void main(String[] args) {
+        int width = 800;
+        int height = width;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setSize(width, height);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GamePanel game = new GamePanel(width, height);
+        frame.add(game);
+        frame.pack();
+        frame.setVisible(true);
+        game.requestFocus();
+    }
+}
