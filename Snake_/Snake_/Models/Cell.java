ce1d8f6019f127e15babfeeb8cdca829518c6b1f
Mintmile <padalicapavel@gmail.com>
2026-09-10 14:18:20 +0300

    Update Cell.java

--- Snake_/Snake_/Models/Cell.java
+++ Snake_/Snake_/Models/Cell.java
@@ -1,1 +1,18 @@
+package Models;
 
+/**
+ * Модель клетки игрового поля (голова, части тела, еда).
+ */
+public class Cell {
+    public int x;
+    public int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean equals(Cell other) {
+        return this.x == other.x && this.y == other.y;
+    }
+}
